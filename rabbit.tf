696aa5853092d3c8ce07bfcbefb6bda207f7483d
Kristin0 <kris123kris99@gmail.com>
2021-07-07 01:42:21 +0400

    first commit

---
+++ rabbit.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "us-east-1"
+    
+}
+
+resource "aws_instance" "rmq" {
+    ami = "ami-07ebfd5b3428b6f4d"
+    instance_type = "t2.micro"
+    key_name = "A4L"
+    vpc_security_group_ids = ["sg-0e9ca0f3d589f7bbb"]
+
+    tags = {
+        Name = rabbit
+    }
+}
